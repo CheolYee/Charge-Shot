e666f614400463da68ff8a00630e7af578381c20
lusalord <lusalord63@gmail.com>
2025-10-06 00:31:55 +0900

    fix : electrode UI

--- Assets/00.Work/WorkSpace/Lusalord/02.Script/Main/SwitchMode.cs
+++ Assets/00.Work/WorkSpace/Lusalord/02.Script/Main/SwitchMode.cs
@@ -1,17 +1,22 @@
+using System;
 using UnityEngine;
 
 public class SwitchMode : MonoBehaviour
 {
-    public bool switchMode = true;
+    public bool switchMode;
 
     public GameObject switchOn;
     public GameObject switchOff;
 
+    private void Start()
+    {
+        switchMode = false; // off 상태로 시작
+    }
 
     public void OnClickSwitch()
     {
         switchMode = !switchMode;
-        switchOn.SetActive(switchMode);
-        switchOff.SetActive(!switchMode);
+        switchOn.SetActive(switchMode); // On 인거 넣어야함
+        switchOff.SetActive(!switchMode); // Off 인거 넣어야함
     }
 }
